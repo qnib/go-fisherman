f1bfb1c0146d5134f5842b7ba1db70853fcd6087
Christian Kniep <ckniep@gaikai.com>
2017-05-29 10:21:49 +0200

    Bump version: 0.0.3 → 0.0.4

--- main.go
+++ main.go
@@ -198,7 +198,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Golang library to help fishing information from moby"
 	app.Usage = "go-fishermen [options] <service>"
-	app.Version = "0.0.3"
+	app.Version = "0.0.4"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "out,o",
